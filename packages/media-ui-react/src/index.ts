0f6156ff7489cb57783f458d8608920f61ba135a
VINAY KUMAR MAHTO <120046338+Vinay980@users.noreply.github.com>
2026-08-11 16:22:03 +0530

    feat(ui-react): export headless MediaGrid hook

--- packages/media-ui-react/src/index.ts
+++ packages/media-ui-react/src/index.ts
@@ -1,43 +1,22 @@
-export {
-  MediaCard,
-} from "./components/MediaCard.js";
+export { MediaCard } from "./components/MediaCard.js";
+export { MediaGrid } from "./components/MediaGrid.js";
+export { MediaSearch } from "./components/MediaSearch.js";
+export { MediaLightbox } from "./components/MediaLightbox.js";
+export { MediaReel } from "./components/MediaReel.js";
+
+export type { MediaCardProps } from "./components/MediaCard.js";
+export type { MediaGridProps } from "./components/MediaGrid.js";
+export type { MediaSearchProps } from "./components/MediaSearch.js";
+export type { MediaLightboxProps } from "./components/MediaLightbox.js";
+export type { MediaReelProps } from "./components/MediaReel.js";
+export type { MediaItem } from "./types/media.js";
 
 export {
-  MediaGrid,
-} from "./components/MediaGrid.js";
-
-export {
-  MediaSearch,
-} from "./components/MediaSearch.js";
+  useMediaGrid,
+} from "./hooks/useMediaGrid.js";
 
 export type {
-  MediaCardProps,
-} from "./components/MediaCard.js";
-
-export type {
-  MediaGridProps,
-} from "./components/MediaGrid.js";
-
-export type {
-  MediaSearchProps,
-} from "./components/MediaSearch.js";
-
-export type {
-  MediaItem,
-} from "./types/media.js";
-
-export {
-  MediaLightbox,
-} from "./components/MediaLightbox.js";
-
-export type {
-  MediaLightboxProps,
-} from "./components/MediaLightbox.js";
-
-export {
-  MediaReel,
-} from "./components/MediaReel.js";
-
-export type {
-  MediaReelProps,
-} from "./components/MediaReel.js";
+  UseMediaGridOptions,
+  MediaGridRootProps,
+  MediaGridItemProps,
+} from "./hooks/useMediaGrid.js";
